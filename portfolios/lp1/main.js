85dd7556e30f54f7dc1626b3d4dc6c89f934315a
chihokyo <chihoukyo@gmail.com>
2021-10-22 14:53:42 +0900

    增加作品集1&微调主页

---
+++ portfolios/lp1/main.js
@@ -0,0 +1,9 @@
+/*==================== SCROLLREVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "10px",
+  duration: 2500,
+  delay: 400,
+});
+
+sr.reveal(".content");
